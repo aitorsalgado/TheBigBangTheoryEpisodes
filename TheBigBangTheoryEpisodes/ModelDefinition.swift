c877b2221a0ed9728670d678db0303002b9c368b
Aitor Salgado <aitorsalgado6@icloud.com>
2023-02-14 19:04:43 +0100

    Add model data for Favorite episodes. Also, add load and save methods.

--- TheBigBangTheoryEpisodes/ModelDefinition.swift
+++ TheBigBangTheoryEpisodes/ModelDefinition.swift
@@ -23,3 +23,7 @@ struct BigBang: Codable, Identifiable, Hashable {
 extension BigBang {
     static let episodeTest = BigBang(id: 2961, url: URL(string: "https://www.tvmaze.com/episodes/2961/the-big-bang-theory-3x09-the-vengeance-formulation")!, name: "The Vengeance Formulation", season: 3, number: 9, airdate: "2009-11-23", runtime: 30, image: "12474", summary: "After he's humiliated on National Public Radio, Sheldon vows to destroy Kripke, while Wolowitz tries not to destroy his new relationship with Bernadette.\n")
 }
+
+struct FavoritesEpisodes: Codable {
+    let favoritesIDs:[Int]
+}
--- TheBigBangTheoryEpisodes/ModelPersistance.swift
+++ TheBigBangTheoryEpisodes/ModelPersistance.swift
@@ -10,6 +10,7 @@ import Foundation
 extension URL {
     static let episodesInitDataURL = Bundle.main.url(forResource: "BigBang", withExtension: "json")!
     static let episodesDocDataURL = URL.documentsDirectory.appending(component: "espisodesBigBangData").appendingPathExtension("json")
+    static let episodesFavIdsDocURL = URL.documentsDirectory.appending(component: "espisodesFavoritesBigBangData").appendingPathExtension("json")
 }
 
 final class ModelPersistance {
@@ -36,6 +37,27 @@ final class ModelPersistance {
             try data.write(to: url, options: [.atomic,.completeFileProtection])
         } catch {
             print("Error save json data: \(error)")
+        }
+    }
+    
+    func loadFavorites() -> FavoritesEpisodes? {
+        do {
+            var urlFavData = URL.episodesFavIdsDocURL
+            let data = try Data(contentsOf: urlFavData)
+            return try JSONDecoder().decode(FavoritesEpisodes.self, from: data)
+        } catch {
+            print("Error loading Favorites from JSON data: \(error)")
+            return nil
+        }
+    }
+    
+    func saveFavorites(favorites:FavoritesEpisodes) {
+        let urlSaveFavorites = URL.episodesFavIdsDocURL
+        do {
+            let dataSave = try JSONEncoder().encode(favorites)
+            try dataSave.write(to: urlSaveFavorites, options: [.atomic,.completeFileProtection])
+        } catch {
+            print("Error save JSON favorites data: \(error)")
         }
     }
 }
